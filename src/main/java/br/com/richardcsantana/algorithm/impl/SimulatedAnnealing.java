93e991738ea3b58478ff429570280698e5a60a40
RichardCSantana <richardcsantana@gmail.com>
2016-03-01 23:18:08 -0300

    arquivo de configuração gerado

--- src/main/java/br/com/richardcsantana/algorithm/impl/SimulatedAnnealing.java
+++ src/main/java/br/com/richardcsantana/algorithm/impl/SimulatedAnnealing.java
@@ -21,74 +21,74 @@ import br.com.richardcsantana.model.Neighbour;
 @Component
 public class SimulatedAnnealing implements Algorithm {
 
-	private final NeighbourGenerator generator;
-	private final Avaliation avaliation;
+    private final NeighbourGenerator generator;
+    private final Avaliation avaliation;
 
-	@Value("${initialTemperature:0}")
-	private double initialTemperature;
-	@Value("${maxSuccessPerIteration:100}")
-	private int maxSuccessPerIteration;
-	@Value("${maxGenerationsPerIteration:100}")
-	private int maxGenerationsPerIteration;
-	@Value("${temperatureReductionFactor:0.2}")
-	private double temperatureReductionFactor;
-	@Value("${maxIterations:100}")
-	private int maxIterations;
+    @Value("${initialTemperature:0}")
+    private double initialTemperature;
+    @Value("${maxSuccessPerIteration:100}")
+    private int maxSuccessPerIteration;
+    @Value("${maxGenerationsPerIteration:1000}")
+    private int maxGenerationsPerIteration;
+    @Value("${temperatureReductionFactor:0.2}")
+    private double temperatureReductionFactor;
+    @Value("${maxIterations:10000}")
+    private int maxIterations;
 
-	@Autowired
-	public SimulatedAnnealing(final NeighbourGenerator generator, final Avaliation avaliation) {
-		this.generator = generator;
-		this.avaliation = avaliation;
-	}
+    @Autowired
+    public SimulatedAnnealing(final NeighbourGenerator generator, final Avaliation avaliation) {
+        this.generator = generator;
+        this.avaliation = avaliation;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see
-	 * br.com.richardcsantana.algorithm.Algorithm#run(br.com.richardcsantana.
-	 * model.Neighbour)
-	 */
-	public Neighbour run(final Neighbour inicialState) {
-		Neighbour currentState = inicialState;
-		double currentTemperature = initialTemperature;
-		int i = 1;
-		int numberSuccess = 0;
-		final Random random = new Random();
-		do {
-			int j = 1;
-			numberSuccess = 0;
-			do {
-				final Neighbour newState = generator.generate(8);
-				final int avaliationNewState = avaliation.avaliate(newState);
-				final int avaliationCurrentState = avaliation.avaliate(currentState);
-				final int delta = avaliationNewState - avaliationCurrentState;
-				System.out.println(String.format("Iteration %d, generation %d", i, j));
-				System.out.println(String.format("avaliation: currentState %d, newState %d", avaliationCurrentState,
-						avaliationNewState));
-				System.out.println(String.format("delta %d", delta));
-				System.out.println(String.format("success %d", numberSuccess));
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * br.com.richardcsantana.algorithm.Algorithm#run(br.com.richardcsantana.
+     * model.Neighbour)
+     */
+    public Neighbour run(final Neighbour inicialState) {
+        Neighbour currentState = inicialState;
+        double currentTemperature = initialTemperature;
+        int i = 1;
+        int numberSuccess = 0;
+        final Random random = new Random();
+        do {
+            int j = 1;
+            numberSuccess = 0;
+            do {
+                final Neighbour newState = generator.generate(8);
+                final int avaliationNewState = avaliation.avaliate(newState);
+                final int avaliationCurrentState = avaliation.avaliate(currentState);
+                final int delta = avaliationNewState - avaliationCurrentState;
+                System.out.println(String.format("Iteration %d, generation %d", i, j));
+                System.out.println(String.format("avaliation: currentState %d, newState %d", avaliationCurrentState,
+                        avaliationNewState));
+                System.out.println(String.format("delta %d", delta));
+                System.out.println(String.format("success %d", numberSuccess));
 
-				final double randomDouble = random.nextDouble();
-				final double calculation = Math.pow(Math.E, (-delta / currentTemperature));
-				final boolean changeValues = changeValues(delta, randomDouble, calculation);
-				if (changeValues) {
-					System.out.println(
-							String.format("values: exponetial^(delta/currentTemperature) %f, random: %f, change %b",
-									calculation, randomDouble, changeValues));
-					currentState = newState;
-					numberSuccess++;
-				}
-				j++;
-			} while ((numberSuccess < maxSuccessPerIteration) && (j <= maxGenerationsPerIteration));
-			currentTemperature *= temperatureReductionFactor;
-			System.out.println(String.format("currentTemperature %f", currentTemperature));
-			i++;
-		} while ((numberSuccess > 0) && (i <= maxIterations));
-		return currentState;
-	}
+                final double randomDouble = random.nextDouble();
+                final double calculation = Math.pow(Math.E, (-delta / currentTemperature));
+                final boolean changeValues = changeValues(delta, randomDouble, calculation);
+                if (changeValues) {
+                    System.out.println(
+                            String.format("values: exponetial^(delta/currentTemperature) %f, random: %f, change %b",
+                                    calculation, randomDouble, changeValues));
+                    currentState = newState;
+                    numberSuccess++;
+                }
+                j++;
+            } while ((numberSuccess < maxSuccessPerIteration) && (j <= maxGenerationsPerIteration));
+            currentTemperature *= temperatureReductionFactor;
+            System.out.println(String.format("currentTemperature %f", currentTemperature));
+            i++;
+        } while ((numberSuccess > 0) && (i <= maxIterations));
+        return currentState;
+    }
 
-	private boolean changeValues(final int delta, final double randomDouble, final double calculation) {
-		return (delta <= 0) || (calculation > randomDouble);
-	}
+    private boolean changeValues(final int delta, final double randomDouble, final double calculation) {
+        return (delta <= 0) || (calculation > randomDouble);
+    }
 
 }
